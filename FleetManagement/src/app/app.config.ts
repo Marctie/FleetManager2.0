44b4aaa551eb3ddddb437d576ccb06709bbca098
Marco Peluso <marco.peluso@intellitronika.com>
2025-10-31 16:05:11 +0100

    correction

--- FleetManagement/src/app/app.config.ts
+++ FleetManagement/src/app/app.config.ts
@@ -1,4 +1,5 @@
 import {
+  APP_INITIALIZER,
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
@@ -6,8 +7,13 @@ import {
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { ConfigService } from './services/config.service';
 
 import { routes } from './app.routes';
+
+export function initializeApp(configService: ConfigService) {
+  return () => configService.loadConfig().toPromise();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +21,12 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    // Carica config.json PRIMA di tutto
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [ConfigService],
+      multi: true,
+    },
   ],
 };
